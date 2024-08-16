fd30a123a21bc45ae7923efa9d853ee53a0fcfad
scatter27-lab <chyvashok98@gmail.com>
2024-08-16 18:53:43 +0300

    first step, add script gendiff

---
+++ difficult_calculator/scripts/gendiff.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="gendiff",
+        description="Compares two configuration files and shows a difference.")
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    #parser.add_argument_group("first_file" | "second_file")
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
