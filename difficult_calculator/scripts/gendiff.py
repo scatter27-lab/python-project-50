eab0f5e345d51d01ab6d1f8c42352f1849dd3d57
scatter27-lab <chyvashok98@gmail.com>
2024-08-16 19:22:42 +0300

    step 2, changed gendiff.py

--- difficult_calculator/scripts/gendiff.py
+++ difficult_calculator/scripts/gendiff.py
@@ -7,6 +7,7 @@ def main():
         description="Compares two configuration files and shows a difference.")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
+    parser.add_argument("-f", "--format", help="set format of output")
     #parser.add_argument_group("first_file" | "second_file")
     args = parser.parse_args()
 
