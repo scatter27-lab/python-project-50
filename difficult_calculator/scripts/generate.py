92d0ed437c4c145165f8f14cf71309c0511e998e
scatter27-lab <chyvashok98@gmail.com>
2024-08-27 07:53:34 +0300

    fix generate_diff

--- difficult_calculator/scripts/generate.py
+++ difficult_calculator/scripts/generate.py
@@ -1,7 +1,6 @@
 import json
 from gendiff import generate_diff
 
-diff = generate_diff(json.load(open('../files/file1.json')),
-                     json.load(open('../files/file2.json')))
-#'difficult_calculator/files/file1.json' 'difficult_calculator/files/file2.json'
+diff = generate_diff('../files/file1.json',
+                     '../files/file2.json')
 print(diff)
