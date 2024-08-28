d6d345aa61c643d30e90eed1d7b53c7fafed3d03
Alexandr Sergeev <chyvashok98@gmail.com>
2024-08-28 10:03:18 +0300

    tests №2

--- tests/test_different.py
+++ tests/test_different.py
@@ -2,4 +2,4 @@ import difficult_calculator
 from difficult_calculator.code.compare import generate_diff
 
 assert (generate_diff('fixtures/file_different1.json','fixtures/file_different2.json') ==
-        f'{{\n + follow: false\n - proxy: 123.234.53.22 \n}}')
+        f'{{\n + follow: False\n - proxy: 123.234.53.22\n}}')
